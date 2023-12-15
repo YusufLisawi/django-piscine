0f976c9e6f930ca209cddd15d2166300d766ec3a
YusufLisawi <yusufisawi@gmail.com>
2023-12-15 20:03:26 +0100

    Updates

--- d04-[django-basics]/d04/settings.py
+++ d04-[django-basics]/d04/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -124,3 +123,70 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+HISTORY_LOG_FILE = BASE_DIR / 'ex02/history.log'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'django.server': {
+            '()': 'django.utils.log.ServerFormatter',
+            'format': '[{server_time}] {message}',
+            'style': '{',
+        },
+        'history_format': {
+            '()': 'django.utils.log.ServerFormatter',
+            'format': '[{asctime}] {message}',
+            'style': '{',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'django.server': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'django.server',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'history_handler': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': HISTORY_LOG_FILE,
+            'mode': 'a',
+            'formatter': 'history_format',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'mail_admins'],
+            'level': 'INFO',
+        },
+        'django.server': {
+            'handlers': ['django.server'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'history': {
+            'handlers': ['console', 'history_handler'],
+            'level': 'INFO'
+        }
+    }
+}
--- d04-[django-basics]/ex02/views.py
+++ d04-[django-basics]/ex02/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import forms
+import logging
+from django.conf import settings
 
 # Create your views here.
-history = []
-
 def index(request):
-    global history
+    logger = logging.getLogger('history')
 
     if request.method == 'POST':
         form = forms.MyForm(request.POST)
         if form.is_valid():
             field_value = form.cleaned_data['field']
-            history.append(field_value)
+            logger.info(field_value)
     else:
         form = forms.MyForm()
+    try:
+        print(settings.HISTORY_LOG_FILE)
+        with open(settings.HISTORY_LOG_FILE, 'r') as f:
+            history = [line for line in f.readlines()]
+    except:
+        history = []
     return render(request, 'ex02/index.html', {'form' : form, 'history' : history})
