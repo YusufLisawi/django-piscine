547b30b4c566d3b39175c1e6fab81dcc4b5bfd90
YusufLisawi <yusufisawi@gmail.com>
2023-12-17 23:49:57 +0100

    Update pip install command in setup.sh

---
+++ d06-[django-auth]/tips/middleware/session.py
@@ -0,0 +1,26 @@
+from django.conf import settings
+from django.http import HttpRequest
+from django.utils.deprecation import MiddlewareMixin
+import time
+import random
+
+SESSION_EXFIRE_TIME = 42
+SESSION_TIMEOUT_KEY = "_session_anonmymous_timestamp_"
+
+class AnonymousSessionMiddleware(MiddlewareMixin):
+    def process_request(self, request: HttpRequest):
+        if request.user.is_authenticated:
+            return
+
+        init_time = request.session.setdefault(
+            SESSION_TIMEOUT_KEY, time.time())
+
+        session_is_expired = time.time() - init_time > SESSION_EXFIRE_TIME
+
+        if session_is_expired:
+            request.session.flush()
+
+        request.session.setdefault('anonymous', random.choice(
+            settings.ANONYMOUS_NAMES))
+
+        request.user.username = request.session.get('anonymous')
--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ VENV_DIR=".venv"
 python3 -m venv $VENV_DIR
 source $VENV_DIR/bin/activate
 
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r $HOME/django-piscine/requirements.txt
